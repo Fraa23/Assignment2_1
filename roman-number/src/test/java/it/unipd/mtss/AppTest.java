cb844a5b449ef976704a212491a32e3924dee293
Francesco <francesco.lapenna.1@studenti.unipd.it>
2024-05-09 16:37:49 +0200

    configurazione base checkstyle Close#16, configurazione modulo Header Close#13

--- roman-number/src/test/java/it/unipd/mtss/AppTest.java
+++ roman-number/src/test/java/it/unipd/mtss/AppTest.java
@@ -1,3 +1,8 @@
+////////////////////////////////////////////////////////////////////
+// Sara Ristovic 2080101
+// Francesco Lapenna 2072134
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.mtss;
 
 import static org.junit.Assert.assertTrue;
@@ -7,14 +12,12 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 }
