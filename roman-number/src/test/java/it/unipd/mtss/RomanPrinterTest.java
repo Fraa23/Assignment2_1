9912500c60c2d12a7fa124d963dc4b3206be807e
Sara <ristovicsara999@gmail.com>
2024-05-16 14:28:26 +0200

    alcune modifiche

--- roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -7,60 +7,55 @@ import org.junit.Test;
 public class RomanPrinterTest {
     @Test
     public void testPrint_basicSymbols() {
-       assertEquals(RomanPrinter.print(1),
-               "  _____  \n" +
-               " |_   _| \n" +
-               "   | |   \n" +
-               "   | |   \n" +
-               "  _| |_  \n" +
-               " |_____| \n" );
+        String I = "  _____  \n" +
+                " |_   _| \n" +
+                "   | |   \n" +
+                "   | |   \n" +
+                "  _| |_  \n" +
+                " |_____| \n";
+        String V = " __      __ \n" +
+                " \\ \\    / / \n" +
+                "  \\ \\  / /  \n" +
+                "   \\ \\/ /   \n" +
+                "    \\  /    \n" +
+                "     \\/     \n";
+        String X = " __   __ \n" +
+                " \\ \\ / / \n" +
+                "  \\ V /  \n" +
+                "   > <   \n" +
+                "  / . \\  \n" +
+                " /_/ \\_\\ \n";
+        String L = "  _       \n" +
+                " | |      \n" +
+                " | |      \n" +
+                " | |      \n" +
+                " | |____  \n" +
+                " |______|  \n";
+        String C = "   _____  \n" +
+                "  / ____| \n" +
+                " | |      \n" +
+                " | |      \n" +
+                " | |____  \n" +
+                "  \\_____|  \n";
+        String D = "  _____   \n" +
+                " |  __ \\  \n" +
+                " | |  | | \n" +
+                " | |  | | \n" +
+                " | |__| | \n" +
+                " |_____/   \n";
+        String M = "  __  __  \n" +
+                " |  \\/  | \n" +
+                " | \\  / | \n" +
+                " | |\\/| | \n" +
+                " | |  | | \n" +
+                " |_|  |_|  \n";
 
-       assertEquals(RomanPrinter.print(5),
-               " __      __ \n" +
-                       " \\ \\    / / \n" +
-                       "  \\ \\  / /  \n" +
-                       "   \\ \\/ /   \n" +
-                       "    \\  /    \n" +
-                       "     \\/     \n");
-
-       assertEquals(RomanPrinter.print(10),
-               " __   __ \n" +
-                       " \\ \\ / / \n" +
-                       "  \\ V /  \n" +
-                       "   > <   \n" +
-                       "  / . \\  \n" +
-                       " /_/ \\_\\ \n");
-
-       assertEquals(RomanPrinter.print(50),
-                 "  _       \n" +
-                        " | |      \n" +
-                        " | |      \n" +
-                        " | |      \n" +
-                        " | |____  \n" +
-                        " |______|  \n");
-
-       assertEquals(RomanPrinter.print(100),
-                "   _____  \n" +
-                        "  / ____| \n" +
-                        " | |      \n" +
-                        " | |      \n" +
-                        " | |____  \n" +
-                        "  \\_____|  \n" );
-
-       assertEquals(RomanPrinter.print(500),
-                "  _____   \n" +
-                        " |  __ \\  \n" +
-                        " | |  | | \n" +
-                        " | |  | | \n" +
-                        " | |__| | \n" +
-                        " |_____/   \n" );
-
-       assertEquals(RomanPrinter.print(1000),
-               "  __  __  \n" +
-                       " |  \\/  | \n" +
-                       " | \\  / | \n" +
-                       " | |\\/| | \n" +
-                       " | |  | | \n" +
-                       " |_|  |_|  \n" );
+        assertEquals(I, RomanPrinter.print(1));
+        assertEquals(V, RomanPrinter.print(5));
+        assertEquals(X, RomanPrinter.print(10));
+        assertEquals(L, RomanPrinter.print(50));
+        assertEquals(C, RomanPrinter.print(100));
+        assertEquals(D, RomanPrinter.print(500));
+        assertEquals(M, RomanPrinter.print(1000));
     }
 }
