e3ad7eb9a5c35655cde23830ee48a4d5e8a9b5e7
Francesco <francesco.lapenna.1@studenti.unipd.it>
2024-05-16 15:53:39 +0200

    bozza di codice per IntegerToRoman e RomanPrinter

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -5,12 +5,88 @@
 
 package it.unipd.mtss;
 
+import it.unipd.mtss.exception.OutOfRange;
+
 public class RomanPrinter {
     public static String print(int num) {
-        return printAsciiArt(IntegerToRoman.convert(num));
+        try {
+            return printAsciiArt(IntegerToRoman.convert(num));
+        }
+        catch (OutOfRange e) {
+            System.out.println("Input out of range.");
+            return null;
+        }
     }
     private static String printAsciiArt(String romanNumber) {
-        //TODO
-        return null;
+        String I = "  _____  \n" +
+                " |_   _| \n" +
+                "   | |   \n" +
+                "   | |   \n" +
+                "  _| |_  \n" +
+                " |_____| \n";
+        String V = " __      __ \n" +
+                " \\ \\    / / \n" +
+                "  \\ \\  / /  \n" +
+                "   \\ \\/ /   \n" +
+                "    \\  /    \n" +
+                "     \\/     \n";
+        String X = " __   __ \n" +
+                " \\ \\ / / \n" +
+                "  \\ V /  \n" +
+                "   > <   \n" +
+                "  / . \\  \n" +
+                " /_/ \\_\\ \n";
+        String L = "  _       \n" +
+                " | |      \n" +
+                " | |      \n" +
+                " | |      \n" +
+                " | |____  \n" +
+                " |______|  \n";
+        String C = "   _____  \n" +
+                "  / ____| \n" +
+                " | |      \n" +
+                " | |      \n" +
+                " | |____  \n" +
+                "  \\_____|  \n";
+        String D = "  _____   \n" +
+                " |  __ \\  \n" +
+                " | |  | | \n" +
+                " | |  | | \n" +
+                " | |__| | \n" +
+                " |_____/   \n";
+        String M = "  __  __  \n" +
+                " |  \\/  | \n" +
+                " | \\  / | \n" +
+                " | |\\/| | \n" +
+                " | |  | | \n" +
+                " |_|  |_|  \n";
+
+        StringBuilder aux = new StringBuilder();
+        for(int i = 0; i < romanNumber.length(); i++) {
+            if (romanNumber.charAt(i) == 'I') {
+                aux.append(I);
+            }
+            else if (romanNumber.charAt(i) == 'V') {
+                aux.append(V);
+            }
+            else if (romanNumber.charAt(i) == 'X') {
+                aux.append(X);
+            }
+            else if (romanNumber.charAt(i) == 'L') {
+                aux.append(L);
+            }
+            else if (romanNumber.charAt(i) == 'C') {
+                aux.append(C);
+            }
+            else if (romanNumber.charAt(i) == 'D') {
+                aux.append(D);
+            }
+            else if (romanNumber.charAt(i) == 'M') {
+                aux.append(M);
+            }
+        }
+
+        System.out.println(aux.toString());
+        return aux.toString();
     }
 }
